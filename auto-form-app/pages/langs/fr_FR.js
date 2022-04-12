50863fb18ae3db67c4844ea86ace13a2251dabdc
maria.bohorquezcaro <maria.bohorquezcaro@telecom-paris.fr>
2022-04-12 14:37:50 +0200

    ajout majuscules

--- auto-form-app/pages/langs/fr_FR.js
+++ auto-form-app/pages/langs/fr_FR.js
@@ -1,133 +1,133 @@
 export const fr_FR = {
     //menu principal 
-    "identity-papers-citizenship": "identité - papiers - citoyenneté",
-    "family-dependent": "famille - personnes à charge",
-    "transports": "moyens de locomotion",
-    "work-unemployment-income": "emploi-chômage-revenus",
-    "housing": "logement",
-    "taxes-money": "impots - argent",
-    "healthcare": "social - santé",
-    "leisure": "loisirs",
-    "foreigner": "étrangers",
-    "identification": "numéros d'identification",
+    "identity-papers-citizenship": "Identité - papiers - citoyenneté",
+    "family-dependent": "Famille - personnes à charge",
+    "transports": "Moyens de locomotion",
+    "work-unemployment-income": "Emploi-chômage-revenus",
+    "housing": "Logement",
+    "taxes-money": "Impots - argent",
+    "healthcare": "Social - santé",
+    "leisure": "Loisirs",
+    "foreigner": "Étrangers",
+    "identification": "Numéros d'identification",
 
     // Identité - Papiers - Citoyenneté : Généralités 
-    "general": "généralités",
-    "lastName": "nom",
-    "firstName": "prénom",
-    "birthday": "date de naissance",
-    "gender": "genre",
-    "nationality": "nationalité",
-    "french_Id": "carte nationale d'identité française",
+    "general": "Généralités",
+    "lastName": "Nom",
+    "firstName": "Prénom",
+    "birthday": "Date de naissance",
+    "gender": "Genre",
+    "nationality": "Nationalité",
+    "french_Id": "Carte nationale d'identité française",
     "foreign_Id": "Carte d'identité étrangère",
-    "french_passport": "passeport français",
-    "foreign_passport": "passeport étranger",
-    "??": "carte de séjour temporaire",
-    "visa": "visa",
-    "residency_card": "carte de résident.e",
+    "french_passport": "Passeport français",
+    "foreign_passport": "Passeport étranger",
+    "??": "Carte de séjour temporaire",
+    "visa": "Visa",
+    "residency_card": "Carte de résident.e",
     "residency_card_UE": "Carte de ressortissant d'un État membre de l'Union européenne ou de l'Espace économique européen",
     "birth_certificate": "Acte de naissance",
 
     // Identité - Papiers - Citoyenneté : contact 
-    "contact": "contact",
-    "main_adress": "adresse principale",
-    "name": "nom sur la boite aux lettres",
-    "adress": "adresse",
-    "ZIP_code": "code postal",
-    "student_adress": "adresse étudiante",
-    "email": "mail",
-    "cellphone_number": "téléphone portable",
-    "phone_number": "téléphone fixe",
+    "contact": "Contact",
+    "main_adress": "Adresse principale",
+    "name": "Nom sur la boite aux lettres",
+    "adress": "Adresse",
+    "ZIP_code": "Code postal",
+    "student_adress": "Adresse étudiante",
+    "email": "Mail",
+    "cellphone_number": "Téléphone portable",
+    "phone_number": "Téléphone fixe",
 
     //Famille et personnes à charge 
-    "booklet": "livrets", //ss
-    "health_book": "carnet de santé",
-    "family_book": "livret de famille",
-    "military_book": "livret militaire",
-    "partner": "compagnon/compagne",//ss
-    "mariage": "contrat de mariage ou pax",
-    "divorce_cecree": "jugement de divorce",
-    "kids_Id": "identité des enfants ",//ss
-    "kid": "enfant",
-    "dependent_Id": "identité de personne à charge",//ss
-    "??": "ordonnance de non conciliation",
+    "booklet": "Livrets", //ss
+    "health_book": "Carnet de santé",
+    "family_book": "Livret de famille",
+    "military_book": "Livret militaire",
+    "partner": "Compagnon/compagne",//ss
+    "mariage": "Contrat de mariage ou pax",
+    "divorce_cecree": "Jugement de divorce",
+    "kids_Id": "Identité des enfants ",//ss
+    "kid": "Enfant",
+    "dependent_Id": "Identité de personne à charge",//ss
+    "??": "Ordonnance de non conciliation",
 
     //Transport 
-    "drivers_license": "permis", //ss
-    "french_drivers_license": "permis de conduire français ",
-    "international-drivers_license": "permis de conduire étranger ",
-    "vehicle": "vehicule",//ss
-    "vehicle_insurance": "assurance vehicule",
-    "???": "contrôle technique",
+    "drivers_license": "Permis", //ss
+    "french_drivers_license": "Permis de conduire français ",
+    "international-drivers_license": "Permis de conduire étranger ",
+    "vehicle": "Vehicule",//ss
+    "vehicle_insurance": "Assurance vehicule",
+    "???": "Contrôle technique",
     "????": "Carte grise ou certificat d'immatriculation",
-    "public_transports": "transports en commun",//ss
-    "transport_membership": "abonnement pour les transports en commun",
-    "elderly_card": "carte mobilité inclusion mention stationnement",
+    "public_transports": "Transports en commun",//ss
+    "transport_membership": "Abonnement pour les transports en commun",
+    "elderly_card": "Carte mobilité inclusion mention stationnement",
 
     //travail, chomage, revenus
-    "schooling": "scolarité",//ss
-    "school_certificate": "certificat de scolarité",
-    "student_card": "carte étudiante",
-    "european_student_Id": "numéro d'étudiant européen",
-    "degrees": "diplômes", // rmq il faut que chaque utilisateur puisse les rejouter 
-    "professional_activity": "activité professionnelle",//ss
-    "employment_certificats_and_contract": "certificats et contrats de travail",
+    "schooling": "Scolarité",//ss
+    "school_certificate": "Certificat de scolarité",
+    "student_card": "Carte étudiante",
+    "european_student_Id": "Numéro d'étudiant européen",
+    "degrees": "Diplômes", // rmq il faut que chaque utilisateur puisse les rejouter 
+    "professional_activity": "Activité professionnelle",//ss
+    "employment_certificats_and_contract": "Certificats et contrats de travail",
     "ensurance_contract": "Contrats d'assurance",
-    "ensurance_termination_notice": "avis de réception de résiliation des contrats",
-    "unemployment_certificat": "certificat de pôle emploi",
-    "unemployment_benefits": "bulletins d'allocations chômage",
-    "salary_slips": "bulletins de salaires",
-    "family_allowance_payment_slips": "bordereaux de versements des allocations familiales",
-    "scholarship": "bourses",//ss
-    "national_scholarship": "bourse nationale",
-    "international_scholarship": "bourse étrangère",
-    "donations": "donations",
-    "civil_and_militarypension_certificate": "titres de pensions civiles et militaires",
+    "ensurance_termination_notice": "Avis de réception de résiliation des contrats",
+    "unemployment_certificat": "Certificat de pôle emploi",
+    "unemployment_benefits": "Bulletins d'allocations chômage",
+    "salary_slips": "Bulletins de salaires",
+    "family_allowance_payment_slips": "Bordereaux de versements des allocations familiales",
+    "scholarship": "Bourses",//ss
+    "national_scholarship": "Bourse nationale",
+    "international_scholarship": "Bourse étrangère",
+    "donations": "Donations",
+    "civil_and_militarypension_certificate": "Titres de pensions civiles et militaires",
     "CAF": "CAF",
 
     //logement
-    "housing": "logement",//ss
-    "principal_residence": "résidence principale",
-    "ownership_and_notarial_deeds": "actes de propriété et actes notariés",
-    "terminated_leases": "baux résiliés",
-    "current_leases": "baux en cours",
-    "rent_receipt": "quittance de loyer",
-    "home_insurance": "assurance habitation",
-    "accommodation_certificate": "attestation d'hébergement",
-    "proof_of_address": "justificatif de domicile",
-    "social_housing_application_certificate": "attestation de demande de logement social",
-    "domiciliation_certificate": "attestation de domiciliation",
-    "claim_reimbursement_records": "dossiers de remboursements de sinistres",
-    "second_home": "résidence secondaire",//ss
+    "housing": "Logement",//ss
+    "principal_residence": "Résidence principale",
+    "ownership_and_notarial_deeds": "Actes de propriété et actes notariés",
+    "terminated_leases": "Baux résiliés",
+    "current_leases": "Baux en cours",
+    "rent_receipt": "Quittance de loyer",
+    "home_insurance": "Assurance habitation",
+    "accommodation_certificate": "Attestation d'hébergement",
+    "proof_of_address": "Justificatif de domicile",
+    "social_housing_application_certificate": "Attestation de demande de logement social",
+    "domiciliation_certificate": "Attestation de domiciliation",
+    "claim_reimbursement_records": "Dossiers de remboursements de sinistres",
+    "second_home": "Résidence secondaire",//ss
 
     //impots argents
-    "income_tax_receipts": "justificatifs d'impôts sur le revenus",
-    "television_license_fee": "redevance télévision",
-    "proof_of_payment_of_social_security_contributions": "justificatifs de paiement des cotisations de sécurité sociale",
-    "proof_of_payment_of_pension_contributions": "justificatifs de paiement des cotisations de retraite",
-    "proof_of_salary_payment": "justificatifs de paiement des salaires",
-    "proof_of_payment_of_life_annuities": "justificatifs de paiement des rentes viagères",
-    "proof_of_payment_of_alimony": "justificatifs de paiement des pensions alimentaires",
-    "property_tax": "taxe foncière",
-    "audiovisual_license_fee": "redevance audio-visuelle",
+    "income_tax_receipts": "Justificatifs d'impôts sur le revenus",
+    "television_license_fee": "Redevance télévision",
+    "proof_of_payment_of_social_security_contributions": "Justificatifs de paiement des cotisations de sécurité sociale",
+    "proof_of_payment_of_pension_contributions": "Justificatifs de paiement des cotisations de retraite",
+    "proof_of_salary_payment": "Justificatifs de paiement des salaires",
+    "proof_of_payment_of_life_annuities": "Justificatifs de paiement des rentes viagères",
+    "proof_of_payment_of_alimony": "Justificatifs de paiement des pensions alimentaires",
+    "property_tax": "Taxe foncière",
+    "audiovisual_license_fee": "Redevance audio-visuelle",
 
     //social santé
-    "proof_of_sports_aptitude": "justificatif d'aptitude sportive par le médecin traitant",
-    "vital_card": "carte vitale",
-    "certificate_of_entitlement_to_health_insurance": "attestation de droit à l'assurance maladie",
-    "mutual_card": "carte mutuelle",
-    "AME_card": "carte AME",
-    "european_health_insurance_card": "carte européenne d'assurance maladie",
+    "proof_of_sports_aptitude": "Justificatif d'aptitude sportive par le médecin traitant",
+    "vital_card": "Carte vitale",
+    "certificate_of_entitlement_to_health_insurance": "Attestation de droit à l'assurance maladie",
+    "mutual_card": "Carte mutuelle",
+    "AME_card": "Carte AME",
+    "european_health_insurance_card": "Carte européenne d'assurance maladie",
 
     //loisir
-    "boat_license": "permis bateau",
-    "hunting_license": "permis de chasse",
+    "boat_license": "Permis bateau",
+    "hunting_license": "Permis de chasse",
 
     //etranger
-    "residence_permit": "titre de séjour",
-    "accomodation_certificate": "attestation d'accueil",
-    "family_reunion": "regroupement familial",
-    "temporary_residence_permit": "autorisation provisoire de séjour",
+    "residence_permit": "Titre de séjour",
+    "accomodation_certificate": "Attestation d'accueil",
+    "family_reunion": "Regroupement familial",
+    "temporary_residence_permit": "Autorisation provisoire de séjour",
 
 }
 
